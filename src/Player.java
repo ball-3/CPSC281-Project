eaf3fbf7536f565dc07e364184f61fac11bcd118
Katie K <kkat575@icloud.com>
2023-03-09 18:42:21 -0800

    recreated word and player classes after deleting them

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Player {
+    static char guessedLetter;
+    static ArrayList<Integer> correctGuessIndexes;
+    static boolean guessedLetterCorrect;
+    static char[] correctGuesses;
+    static Word word;
+    static Hangman hangmanObject;
+    static int computerDifficulty;
+}
