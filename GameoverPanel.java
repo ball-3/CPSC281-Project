ae0ea5cf8c48b6fb1138c8782f7945db4f268ff3
sleepyKingDog <14cyanorikofuji@gmail.com>
2023-03-23 19:32:32 -0700

    update

--- GameoverPanel.java
+++ GameoverPanel.java
@@ -4,32 +4,33 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class GameoverPanel extends JPanel{
+public class GameoverPanel extends JPanel {
+    private String result;
 
-    public GameoverPanel (){
-            setPreferredSize(new Dimension(1000, 800));
-            JButton restart = new JButton("menu");
-            restart.addActionListener((ActionListener) new ActionListener() {
-                public void actionPerformed(ActionEvent ae) {
-                    //new frame??
-    
-                }
+    public GameoverPanel(boolean clear) {
+        setPreferredSize(new Dimension(1000, 800));
+        JButton restart = new JButton("menu");
+        restart.addActionListener((ActionListener) new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                // new frame??
 
-                
-            });
-            add(restart);
+            }
 
+        });
+        add(restart);
+
+        if (clear) {
+            result = "CLEAR";
+        } else
+            result = "GAME OVER";
 
     }
+
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setFont(new Font("MS Comic Sans", Font.BOLD, 40));
-        g2.drawString("GAME OVER", 100, 100);
-
-
-
+        g2.drawString(result, 100, 100);
 
     }
 
-    
 }
