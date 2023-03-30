ad6c62a86c1868c5395f6526323fe2f755ab39bf
ball-3 <92955909+ball-3@users.noreply.github.com>
2023-03-30 15:39:26 -0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,32 @@
 public class Game {
     
     Word word;
+    Hangman man;
 
     public Game(int[] gamemode)
     {
         //TODO delete these notes
-        //if gamemode[1] == 0;      //picker is computer
-        //word = new word(gamemode[3], gamemode[4])
-
+        if gamemode[1] == 0;      //picker is computer
+        {
+            word = new word(gamemode[3], gamemode[4])
+        }
+        
         //if gamemode[1] == 1;      //picker is human
         //can we add a prompt so user can pick a word here ?
 
         //if gamemode[2] == 0;      //guesser is computer
         //complex case, TODO implement lol
 
-        //if gamemode[2] == 1;      //guesser is human
-        //new Hangman(word);
-    }   
+        if gamemode[2] == 1;      //guesser is human
+        {
+        man = new Hangman(word);
+        }
+    }
+    
+    //TODO fix this, this is basically just copied off the makeshift settings from menu for the hangman
+    private void formatHangman()
+    {
+        man.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        man.setVisible(true);
+    }
 }
