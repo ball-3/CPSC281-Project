53804e4fa94ec0e1bbacf08b32202baa3745686b
ball-3 <92955909+ball-3@users.noreply.github.com>
2023-03-30 15:25:34 -0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,20 @@
 public class Game {
+    
+    Word word;
 
     public Game(int[] gamemode)
     {
+        //TODO delete these notes
+        //if gamemode[1] == 0;      //picker is computer
+        //word = new word(gamemode[3], gamemode[4])
 
-    }
+        //if gamemode[1] == 1;      //picker is human
+        //can we add a prompt so user can pick a word here ?
 
+        //if gamemode[2] == 0;      //guesser is computer
+        //complex case, TODO implement lol
+
+        //if gamemode[2] == 1;      //guesser is human
+        //new Hangman(word);
+    }   
 }
