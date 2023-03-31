e036ab4e834fc2fbc75a4991322e5575d38d4999
ball-3 <92955909+ball-3@users.noreply.github.com>
2023-03-30 17:54:39 -0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,34 +1,37 @@
+import javax.swing.*;
+
 public class Game {
-    
+
     Word word;
     Hangman man;
 
     public Game(int[] gamemode)
     {
         //TODO delete these notes
-        if gamemode[1] == 0;      //picker is computer
+        if (gamemode[1] == 0)     //picker is computer
         {
-            word = new word(gamemode[3], gamemode[4])
+            word = new Word(gamemode[3], gamemode[4]);
         }
-        
-        else if gamemode[1] == 1;      //picker is human
+
+        else if (gamemode[1] == 1)      //picker is human
         {
-        //can we add a prompt so user can pick a word here ?
-        //like a f r a m  e or something
+            //can we add a prompt so user can pick a word here ?
+            //like a f r a m  e or something
         }
-            
-        if gamemode[2] == 0;      //guesser is computer
+
+        if (gamemode[2] == 0)      //guesser is computer
         {
-        //complex case, TODO implement
-        //did katie have a solution for this ?
+            //complex case, TODO implement
+            //did katie have a solution for this ?
         }
-            
-        else if gamemode[2] == 1;      //guesser is human
+
+        else if (gamemode[2] == 1)      //guesser is human
         {
-        man = new Hangman(word);
+            man = new Hangman(word);
         }
+        formatHangman();
     }
-    
+
     //TODO fix this, this is basically just copied off the makeshift settings from menu for the hangman
     private void formatHangman()
     {
@@ -36,4 +39,5 @@ public class Game {
         man.setSize(1000,650);      //TODO is this the final sizee ? no but should be fixed (removed) with the frame fixing
         man.setVisible(true);
     }
+
 }
